4cf126bc0b61886fb869f949604fbdbf4a813e03
harshita <harshita.vuthaluru@gmail.com>
2025-10-06 12:09:45 +0530

    Create App.js

---
+++ REACT/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Greet';
+import Counter from "./Components/Counter";
+import Timer from './Components/Timer';
+import Props from './Components/Props';
+import PasswordStrengthChecker from './Components/PasswordStrengthChecker';
+
+function App() {
+  return (
+    <div>
+      {/* <Greet /> */}
+      {/* <Counter /> */}
+      {/* <Timer /> */}
+      {/*<Props> </Props>*/}
+      <PasswordStrengthChecker/>
+    </div>
+  );
+}
+
+export default App;
