e2429947bab5189aa375d70eacc0f82faf52afd1
harshita <harshita.vuthaluru@gmail.com>
2025-10-06 12:07:41 +0530

    Create props.js

---
+++ REACT/src/components/props.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Media from "./Media";
+
+
+const Props = (props) => {
+    return(
+        <div>
+             <Media name="Harshita" email="harshitha.123@gmail.com" phone="012345" age="28 years" location="Hyderabad"> </Media>
+             <Media name="Ram" email="ram.123@gmail.com" phone="3456789" age="32 years" location="Bangloore"> </Media>
+        </div>
+    )
+} 
+export default Props; 
